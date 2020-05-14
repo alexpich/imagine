63de2827a363276fb86c4d8d556c07f56cadd43e
alexpich <pich.alex01@gmail.com>
2020-05-14 02:34:51 -0700

    Added reducer

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { createStore, combineReducers } from "redux";
 import Navigator from "./navigation/Navigator";
 import { Provider } from "react-redux";
+import imagesReducer from "./store/reducers/imagesReducer";
+
+const rootReducer = combineReducers({
+  images: imagesReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
-  return <Navigator />;
+  return (
+    <Provider store={store}>
+      <Navigator />
+    </Provider>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
